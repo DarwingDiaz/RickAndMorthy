fe3969ff8b89289813ed604c5af04babec4054c6
DarwingDiaz <darwingdiazsanchez@gmail.com>
2023-10-07 09:50:49 -0500

    primer

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import styles from './About.module.css'
+
+function About(){
+    return <div className={styles.Contenedor}>
+        <h1 className={styles.texto}>
+            Elaborado por Darwing Diaz, estudiante de HENRY BOOTCAMP  
+        </h1>
+    </div>
+}
+
+export default About;
